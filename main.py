a6faea90e6cba6054ba8a4012a4594e28ec6f803
kodlulegocu <efruzesena@hotmail.com>
2026-01-09 20:13:45 +0300

    Implement word lookup in zort_dick dictionary
    ok

---
+++ main.py
@@ -0,0 +1,11 @@
+ınız bir kelimeyi yazın (hepsini buyuk harflerle yazın!): ")
+if word in zort_dick.keys():
+    print(zort_dick[word])
+else:
+    print("kediş bu saçma"Garip ya da utandırıcı bir şey",
+            "LOL": "Kzort_dick = {"CRINGE":omik bir şeye verilen cevap",
+            "ROFL" : "bir şakaya karşılık cevap",
+            "SHEESH" : "onaylamamak",
+            "CREEPY" : "korkunç",
+            "AGGRO" : "agresifleşmek/sinirlenmek"}
+word = input("anlamadığ kelime zortta bulunmuyor.")
